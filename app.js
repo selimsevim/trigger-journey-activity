62b6361d69eaba1bb00a8a39f7e3da3fc9994dc8
Selim <selimsevim1@gmail.com>
2024-07-26 12:54:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
