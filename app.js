52a3a98cea3a3cd04bec158c9a37afaac6664740
Selim <selimsevim1@gmail.com>
2024-07-26 12:36:22 +0100

    test

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
