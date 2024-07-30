3d51f7f6df0dfcf30c66d913a05a344a7971c8bf
Selim <selimsevim1@gmail.com>
2024-07-30 15:14:56 +0100

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -33,7 +33,15 @@ define(['postmonger'], function (Postmonger) {
             payload = data;
         }
         connection.trigger('requestSchema');
-        fetchJourneys();
+        const storedJourneys = localStorage.getItem('journeys');
+        const selectedJourneyId = localStorage.getItem('selectedJourneyId');
+
+        if (storedJourneys) {
+            journeys = JSON.parse(storedJourneys);
+            populateJourneys(journeys, selectedJourneyId);
+        } else {
+            fetchJourneys();
+        }
     }
 
     function save() {
@@ -49,6 +57,8 @@ define(['postmonger'], function (Postmonger) {
                     payload: schema
                 }
             ];
+            // Save the selected journey ID to local storage
+            localStorage.setItem('selectedJourneyId', selectedJourney.id);
         }
 
         payload['metaData'].isConfigured = true;
@@ -81,6 +91,7 @@ define(['postmonger'], function (Postmonger) {
                     populateJourneys(journeys);
                     $('#loading-message').hide();
                     $('#journey-checkboxes').show();
+                    localStorage.setItem('journeys', JSON.stringify(journeys));
                 }
             },
             error: function (xhr, status, error) {
@@ -90,24 +101,29 @@ define(['postmonger'], function (Postmonger) {
         });
     }
 
-    function populateJourneys(journeys) {
+    function populateJourneys(journeys, selectedJourneyId = null) {
         let $checkboxGroup = $('#journey-checkboxes');
         $checkboxGroup.empty();
         $checkboxGroup.append('<label>Select Journeys to Monitor:</label>');
 
         journeys.forEach(function (journey) {
             let apiEventKey = journey.defaults.email.find(email => email.includes('APIEvent')).split('"')[1].split('.')[1];
+            let $checkbox = $('<input>', {
+                type: 'checkbox',
+                name: 'journey',
+                value: journey.id,
+                'data-api-event-key': apiEventKey
+            });
+
+            // Check the checkbox if it matches the selected journey ID
+            if (journey.id === selectedJourneyId) {
+                $checkbox.prop('checked', true);
+            }
+
             $checkboxGroup.append(
                 $('<label>', {
                     text: journey.name
-                }).prepend(
-                    $('<input>', {
-                        type: 'checkbox',
-                        name: 'journey',
-                        value: journey.id,
-                        'data-api-event-key': apiEventKey
-                    })
-                )
+                }).prepend($checkbox)
             );
         });
     }
